fbc022375a8be83d8a4645ac64d19db8461dd6b2
hpm <hpm@clix.pt>
2023-09-08 09:55:57 +0200

    improved display of results

--- rgaML/Form1.cs
+++ rgaML/Form1.cs
@@ -33,7 +33,9 @@ namespace rgaML
                 var result = RgaML.MLModel1.Predict(sampleData);
                 Debug.WriteLine($"\n\nPredicted Label value: {result.PredictedLabel} \nPredicted Label scores: [{String.Join(",", result.Score)}]\n\n");
                 label2.Text = result.PredictedLabel;
-                label3.Text = String.Join(",", result.Score);
+                label3.Text = result.Score[0].ToString("00.0%");
+                label4.Text = result.Score[1].ToString("00.0%");
+                label5.Text = result.Score[2].ToString("00.0%");
                 progressBar1.Value = (int)(result.Score[0] * 100);
                 progressBar2.Value = (int)(result.Score[1] * 100);
                 progressBar3.Value = (int)(result.Score[2] * 100);
